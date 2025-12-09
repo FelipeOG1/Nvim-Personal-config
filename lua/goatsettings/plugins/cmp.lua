2401e12c1e7dfc2fb1897dfc72c0c647c2614715
FelipeOG1 <feliog1234@gmail.com>
2025-12-09 16:13:23 -0600

    tab

--- lua/goatsettings/plugins/cmp.lua
+++ lua/goatsettings/plugins/cmp.lua
@@ -1,37 +1,56 @@
 return {
-    {
-        "hrsh7th/nvim-cmp",
-        dependencies = {
-            "hrsh7th/cmp-nvim-lsp",   -- autocompletado desde LSP
-            "hrsh7th/cmp-buffer",     -- autocompletado desde buffer
-            "hrsh7th/cmp-path",       -- autocompletado de rutas
-            "hrsh7th/cmp-cmdline",    -- autocompletado en la línea de comandos
-        },
-        config = function()
-            local cmp = require("cmp")
-            cmp.setup({
-                snippet = {
-                    expand = function(args)
-                        require("luasnip").lsp_expand(args.body)
-                    end,
-                },
-                mapping = cmp.mapping.preset.insert({
-                    ["<C-n>"] = cmp.mapping.select_next_item(),
-                    ["<C-p>"] = cmp.mapping.select_prev_item(),
-                    ["<CR>"] = cmp.mapping.confirm({ select = true }),
-                }),
-                sources = cmp.config.sources({
-                    { name = "nvim_lsp" },
-                    { name = "buffer" },
-                    { name = "path" },
-                }),
-		view = {
-
-		   entries = {max_item_count = 5}
-		
-		}
-            })
-        end,
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",   -- LSP
+      "hrsh7th/cmp-buffer",     -- Buffer
+      "hrsh7th/cmp-path",       -- Rutas
+      "hrsh7th/cmp-cmdline",    -- Cmdline
+      "L3MON4D3/LuaSnip",       -- Snippets
     },
+    config = function()
+      local cmp = require("cmp")
+
+      cmp.setup({
+        preselect = cmp.PreselectMode.Item,
+
+        completion = {
+          completeopt = "menu,menuone,noinsert",
+        },
+
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
+
+        mapping = cmp.mapping.preset.insert({
+          ["<C-n>"] = cmp.mapping.select_next_item(),
+          ["<C-p>"] = cmp.mapping.select_prev_item(),
+
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.confirm({ select = true })
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+
+          ["<CR>"] = cmp.mapping.confirm({ select = false }),
+        }),
+
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "buffer" },
+          { name = "path" },
+        }),
+        view = {
+          entries = {
+            max_item_count = 5,
+          },
+        },
+      })
+    end,
+  },
 }
 
--- lua/goatsettings/plugins/colorschemes.lua
+++ lua/goatsettings/plugins/colorschemes.lua
@@ -7,28 +7,12 @@ return {
         end,
     },
     {
-        "rebelot/kanagawa.nvim",
-        name = "kanagawa",
-        config = function()
-            -- solo se activa si quieres usarlo
-            vim.cmd([[colorscheme kanagawa]])
-	    vim.api.nvim_set_hl(0,"Normal",{bg = "none"})
-	    vim.api.nvim_set_hl(0,"Float",{bg = "none"})
-	    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })  -- para floats
-	    vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })  -- borde de floats
-	    vim.api.nvim_set_hl(0, "VertSplit", { bg = "none" })    -- para splits
-	    vim.api.nvim_set_hl(0, "StatusLine", { bg = "none" })   -- statusline
-	    vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })       -- números de línea
-		    
-	    
-        end,
-    },
-    {
         "EdenEast/nightfox.nvim",
         name = "nightfox",
         config = function()
-            -- vim.cmd([[colorscheme nightfox]])
+             vim.cmd([[colorscheme nightfox]])
         end,
     },
+
+	
 }
